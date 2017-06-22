bae95e5bc1c7acbc8f6520e421fab03e67d477c4
Arthur Taborda <arthur.taborda@glispamedia.com>
2017-06-22 14:57:20 +0200

    travis ci

--- src/test/kotlin/net/artcoder/armada/GameStartTest.kt
+++ src/test/kotlin/net/artcoder/armada/GameStartTest.kt
@@ -1,6 +1,8 @@
 package net.artcoder.armada
 
 import com.google.common.truth.Truth.assertThat
+import net.artcoder.armada.PlayerObjectMother.playerOne
+import net.artcoder.armada.PlayerObjectMother.playerTwo
 import org.testng.annotations.Test
 
 class GameStartTest {
@@ -9,9 +11,10 @@ class GameStartTest {
     when game starts
     player 1 is attacking and player 2 is waiting
      */
-    @Test fun testGameStart() {
-        val playerOne = Player()
-        val playerTwo = Player()
+    @Test
+    fun testGameStart() {
+        val playerOne = playerOne()
+        val playerTwo = playerTwo()
 
         BattleshipGame(playerOne, playerTwo)
 
